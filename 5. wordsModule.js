9a7a1920bf864918e6914fd3e3cf330b7341e9e4
KARTHIKlogu <karthikloganathen@gmail.com>
2024-03-15 11:53:57 +0530

    new updates added

---
+++ 5. wordsModule.js
@@ -0,0 +1,15 @@
+var wordsModule = (function () {
+	//privat property
+	var word = [
+		//In side ower array we have ower three list stoerd as array 
+		"word1 word2 word3 word4 etc",//list 1
+		"word1 word2 word3 word4 etc",// list 2
+		"word1 word2 word3 word4 etc"// list 3
+	];
+	return {
+		getWords: function (textNumber) {// Takest the parameter which will specifices the  number of a particuler list of "word".
+			return word[textNumber];
+		}
+	};
+	
+})();
---
+++ 7. eventsModule.js
@@ -0,0 +1,18 @@
+var eventsModule = (function (dModule, uModule, wModule, cModule) {
+	//This are the arguments of the eventsModule,We used argument heare to add the modules to "INDEX.html" at same time. 
+					
+	var addEventListeners = function () {
+		//charactor typing event listener
+		
+		//click on download button event listner
+		
+		//click on restart button event listener
+	};
+	return {
+		// init function , initializres the  trest ths before start
+		init : function (duration, textNumber) {
+			addEventListeners();
+		}
+	};
+})(dataModule, UIModule, wordsModule, certificateModule);//We used call argument heare to add the modules to "INDEX.html" in a order form. 
+
